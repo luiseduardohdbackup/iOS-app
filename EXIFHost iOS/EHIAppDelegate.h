bc0d1137d96f26f9f6883a0cc9c241f4a2ed8b9d
Andrew Wilcox <AWilcox@Wilcox-Tech.com>
2012-11-02 19:16:50 -0400

    Initial Commit

---
+++ EXIFHost iOS/EHIAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  EHIAppDelegate.h
+//  EXIFHost iOS
+//
+//  Created by Andrew Wilcox on 2012-Nov-02.
+//  Copyright (c) 2012 EXIFHost. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EHIAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ EXIFHost iOS/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  EXIFHost iOS
+//
+//  Created by Andrew Wilcox on 2012-Nov-02.
+//  Copyright (c) 2012 EXIFHost. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "EHIAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([EHIAppDelegate class]));
+	}
+}
