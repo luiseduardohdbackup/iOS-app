2088fe4c90ade8396087fcc90d465a6b4df75eba
Andrew Wilcox <AWilcox@Wilcox-Tech.com>
2012-11-02 20:52:24 -0400

    Add initial authentication VC

---
+++ EXIFHost iOS/EHIAuthViewController.h
@@ -0,0 +1,18 @@
+//
+//  EHIAuthViewController.h
+//  EXIFHost iOS
+//
+//  Created by Andrew Wilcox on 2012-Nov-02.
+//  Copyright (c) 2012 EXIFHost. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EHIAuthViewController : UIViewController <UITextFieldDelegate>
+
+-(void)signInTapped:(id)action;
+
+@property (nonatomic, assign) IBOutlet UITextField *emailField;
+@property (nonatomic, assign) IBOutlet UITextField *passwordField;
+
+@end
---
+++ EXIFHost iOS/EHIAuthViewController.m
@@ -0,0 +1,88 @@
+//
+//  EHIAuthViewController.m
+//  EXIFHost iOS
+//
+//  Created by Andrew Wilcox on 2012-Nov-02.
+//  Copyright (c) 2012 EXIFHost. All rights reserved.
+//
+
+#import "EHIAuthViewController.h"
+
+@interface EHIAuthViewController ()
+
+@end
+
+@implementation EHIAuthViewController
+
+#pragma mark - Properties
+
+@synthesize emailField, passwordField;
+
+
+
+
+#pragma mark - View lifecycle
+
+-(void)viewDidLoad
+{
+	[super viewDidLoad];
+	
+	UIBarButtonItem *signInButton = [[UIBarButtonItem alloc] initWithTitle:@"Sign In"
+									 style:UIBarButtonItemStyleDone
+									target:self
+									action:@selector(signInTapped:)];
+	signInButton.enabled = NO;
+	self.navigationItem.rightBarButtonItem = signInButton;
+	[signInButton release];
+}
+
+
+-(BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+	return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+
+
+
+#pragma mark - Actions
+
+-(void)signInTapped:(id)action
+{
+	[[UIApplication sharedApplication] resignFirstResponder];
+	
+	self.navigationItem.rightBarButtonItem.enabled = NO;
+	self.emailField.enabled = self.passwordField.enabled = NO;
+	
+	// Sign in…
+}
+
+
+
+
+#pragma mark - Text field delegate
+
+-(BOOL)textField:(UITextField *)textField shouldChangeCharactersInRange:(NSRange)range replacementString:(NSString *)string
+{
+	if(self.emailField.text.length > 0 && self.passwordField.text.length > 0)
+		self.navigationItem.rightBarButtonItem.enabled = YES;
+	
+	return YES;
+}
+
+
+-(BOOL)textFieldShouldReturn:(UITextField *)textField
+{
+	if(textField == self.emailField)
+	{
+		[passwordField becomeFirstResponder];
+	}
+	 else if(textField == self.passwordField)
+	{
+		[passwordField resignFirstResponder];
+		[self signInTapped:passwordField];
+	}
+}
+
+
+@end
